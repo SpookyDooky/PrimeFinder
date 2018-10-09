dea4bc1644a6cbc324f7531e516b56e37ea05e78
Raymond <r.danjou@student.tudelft.nl>
2018-10-09 16:41:21 +0200

    Made it a lot faster, can now calculate 6 million primes in a minute

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,13 +5,13 @@ public class Launcher {
 
     public static void main(String[] args){
 
-        WriteHandler.setup("Data\\Version3\\" , "TimeCheck10ThousandPrimes.txt");
+        WriteHandler.setup("Data\\Version4\\" , "6M_Primes.txt");
         double totalSeconds = 0.0;
 
         //Time checking
 
-        for(int times = 0; times < 100;times++) {
-            PrimeAlgorithm algo = new PrimeAlgorithm(10000);
+       /*for(int times = 0; times < 100;times++) {
+            PrimeAlgorithm algo = new PrimeAlgorithm(500000);
             long startTime = System.nanoTime();
             algo.start();
             long endTime = System.nanoTime();
@@ -19,10 +19,15 @@ public class Launcher {
             System.out.println(time);
             WriteHandler.addLog("TIME: " + time);
             totalSeconds += time;
-        }
+        } */
 
-        WriteHandler.addLog(" ");
-        WriteHandler.addLog("AVERAGE TIME: " + String.valueOf(totalSeconds/100.0));
+        PrimeAlgorithm algo = new PrimeAlgorithm(6000000);
+        algo.start();
+
+        //WriteHandler.addLog(" ");
+        //WriteHandler.addLog("AVERAGE TIME: " + String.valueOf(totalSeconds/100.0));
+
+        System.out.println("WRITING");
         WriteHandler.writeLogs();
     }
 }
--- src/main/PrimeAlgorithm.java
+++ src/main/PrimeAlgorithm.java
@@ -10,43 +10,70 @@ public class PrimeAlgorithm {
     private int primesFound = 0;
     private int amountPrimes;
 
-    private ArrayList<Integer> result;
+    private int[] list;
 
-    private HashSet<Integer> history;
+    private int foulSteps = 0;
 
     public PrimeAlgorithm(int amountPrimes){
         this.amountPrimes = amountPrimes;
-        this.result = new ArrayList<>();
-        this.history = new HashSet<>();
+        this.list = new int[amountPrimes];
     }
 
     public void start(){
 
-        for(int x = 2; x < 250000;x++){
+        for(int x = 2; x < 250000000;x++){
             boolean isPrime = true;
             int steps = 0;
 
             int increase = 1;
-            for(int z = 2; z < (int)x; z += increase){
+            for(int z = 2; z < (int)(x * 0.5) + 1; z += increase){
                 steps++;
 
                 if(x % z == 0){
                     isPrime = false;
+                    if(steps > foulSteps) {
+                        foulSteps = steps;
+                    }
                     break;
                 }
 
                 if(z == 3){
                     increase = 2;
                 }
+
+                if (z > 5) {
+                    break;
+                }
+            }
+
+            if(isPrime) {
+                for (int y = 0; y < primesFound; y++) {
+                    if (list[y] > x / 2) {
+                        break;
+                    }
+                    steps++;
+                    if (x % list[y] == 0) {
+                        isPrime = false;
+                        if(steps > foulSteps) {
+                            foulSteps = steps;
+                        }
+                        break;
+                    }
+
+                    if(y > 2000){
+                        break;
+                    }
+                }
             }
 
             if(isPrime){
-                result.add(x);
+                list[primesFound] = x;
                 primesFound++;
-                //WriteHandler.addLog("NUMBER:" + x + ", STEPS: " + steps);
+                WriteHandler.addLog("NUMBER:" + x + ", STEPS: " + steps);
             }
 
             if(amountPrimes == primesFound){
+                System.out.println(foulSteps);
                 break;
             }
         }
